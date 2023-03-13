c0df4543042eda820624fd7d77da808c2f5423ce
Mael Micout <gh api micoutmael3@gmail.com>
2023-03-13 15:47:52 +0100

    setup done

---
+++ app/javascript/packs/home_index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Index from '../pages/home/Index'
+
+const root = ReactDOM.createRoot(document.getElementById('home-index-root'))
+root.render(<Index/>)
